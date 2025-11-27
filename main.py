abc976b7ca00e96d20c3c9cab94d9163dfbca6a8
Sagar Kumar Sethi <sagarkumar.s@enlume.com>
2025-11-27 19:44:15 +0530

    Ëat Food

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import time
 from turtle import Turtle, Screen
+
+from food import Food
 from snake import Snake
 
 screen = Screen()
 snake = Snake()
+food = Food()
 
 screen.setup(600, 600)
 screen.bgcolor("black")
@@ -11,17 +14,18 @@ screen.title("My Snake Game")
 screen.tracer(0)
 screen.listen()
 
-screen.onkey(snake.up,"Up")
-screen.onkey(snake.down,"Down")
-screen.onkey(snake.left,"Left")
-screen.onkey(snake.right,"Right")
-
-
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+    if snake.head.distance(food) < 15:
+        print("nom nom nom")
+        food.refresh()
 
 screen.exitonclick()
